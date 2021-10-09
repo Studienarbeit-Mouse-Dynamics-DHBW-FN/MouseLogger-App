3a688728721c3dc1fcf478f2c24bbe064f003e5c
Moritz Gärtner <moritzgaertner516@gmail.com>
2021-10-09 15:11:18 +0200

    uploader loads data from all files

--- utils/uploader.py
+++ utils/uploader.py
@@ -1,7 +1,9 @@
 from threading import Event, Thread
+import os
 import requests
+import json
 
-from consts import UPLOAD_INTERVALS_IN_S, SEND_URL
+from consts import CLICK_PATH, MOVE_PATH, SCROLL_PATH, UPLOAD_INTERVAL_IN_S, SEND_URL
 
 
 class Uploader:
@@ -22,12 +24,33 @@ class Uploader:
         self._upload.clear()
 
 
+    def get_data(self, path: str) -> list[dict]:
+        data = []
+        for f in os.scandir(path):
+            with open(f, 'r', encoding="UTF-8") as file:
+                data.extend(json.load(file))
+        return data
+
+    def get_move_data(self) -> list[dict]:
+        return self.get_data(MOVE_PATH)
+
+    def get_click_data(self) -> list[dict]:
+        return self.get_data(CLICK_PATH)
+
+    def get_scroll_data(self) -> list[dict]:
+        return self.get_data(SCROLL_PATH)
+
+
     def upload_data(self):
-        while not self._kill.wait(UPLOAD_INTERVALS_IN_S):
+        while not self._kill.wait(UPLOAD_INTERVAL_IN_S):
             if self._upload.is_set():
-                try:
-                    # Daten die hochgeladen sind löschen
-                    res = requests.post(SEND_URL, json={'test': 'x'})
-                    print(res.text)
-                except:
-                    print('err: no data uploaded')
+                self.get_move_data()
+                self.get_click_data()
+                self.get_scroll_data()
+                # # upload data successful -> delete files
+                # try:
+                #     # Daten die hochgeladen sind löschen
+                #     res = requests.post(SEND_URL, json={'test': 'x'})
+                #     print(res.text)
+                # except:
+                #     print('err: no data uploaded')
