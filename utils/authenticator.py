3116ebbadc9aeffc0d3b51e2882b7f1da407a552
Moritz Gärtner <moritzgaertner516@gmail.com>
2021-10-09 15:12:36 +0200

    full auth implemented

--- utils/authenticator.py
+++ utils/authenticator.py
@@ -1,20 +1,61 @@
 import re
+import os
+import json
+import requests
+from threading import Event, Thread
+from getmac import get_mac_address as mac
+
+
+from consts import AUTH_URL, AUTHENTICATION_INTERVAL_IN_S, CONFIG_PATH, FOLDER_PATH
 
 class Authenticator:
-    _authenicated = False
+    _authenticated = False
+    _mail = ""
+
+    _kill = Event()
+    _authenticate = Event()
+
+
+    def __init__(self) -> None:
+        """checks if device is authenticated else start auth thread"""
+        os.makedirs(FOLDER_PATH, exist_ok=True)
+        if os.path.exists(CONFIG_PATH):
+            with open(f"{CONFIG_PATH}", 'r', encoding="UTF-8") as config_file:
+                data = json.load(config_file)
+                self._mail = data["mail"]
+                self._authenticated = True
+            return
+
+        Thread(name=Authenticator, daemon=True, target=self.auth_loop)
+
+    def __delete__(self, _) -> None:
+        self._kill.set()
+
+
+    def is_authenticated(self) -> bool:
+        return self._authenticated
+    
+    def get_mail(self) -> bool:
+        return self._mail
+
 
     def valid_email(self, mail: str) -> bool:
         """return wheter the give mail matches a mail pattern"""
         return re.match(r"[^@]+@[^@]+\.[^@]+", mail)
 
-    def authenticate(self, mail: str) -> None:
-        # url = 'http://localhost:3000/sendmail'
-        # try:
-        #     res = requests.post(url, json={'email': username})
-        #     print(res.text)
-        # except:
-        #     print('err: no username send')
-        pass
-    
-    def is_authenicated(self) -> bool:
-        return self._authenicated
+    def authenticate(self, mail: str) -> bool:
+        self._mail = mail
+        self._authenticate.set()
+
+
+    def auth_loop(self) -> None:
+        while not self._kill.wait(AUTHENTICATION_INTERVAL_IN_S):
+            if self._authenticate.is_set():
+                try:
+                    response = requests.post(AUTH_URL, json=dict(mail=self._mail, id=mac()))
+                    if response.json()["is_verified"]:
+                        self._authenticated = True
+                        self._authenticate.clear()
+                        self._kill.set()
+                finally:
+                    pass
